6a9ec15ceba13722dc860840685a25ed1b96aab6
cross-sky <xtaosky@live.com>
2015-03-12 23:04:14 +0800

    add os.path

---
+++ OsPath.py
@@ -0,0 +1,24 @@
+﻿import os
+
+path = 'C:\DRIVERS'
+
+def travelTree(currentPath, count):
+
+    if not os.path.exists(currentPath):
+        print('path does not exit!')
+        return
+
+    if os.path.isfile(currentPath):
+        fileName = os.path.basename(currentPath)
+        print('t'*count + '|--' + fileName)
+    elif os.path.isdir(currentPath):
+        print('t'*count + '|--' + currentPath)
+        pathList = os.listdir(currentPath)
+        for eachPath in pathList:
+            travelTree(currentPath+ '/' + eachPath,count + 1)
+
+travelTree(path, 1)
+
+'''
+os.path basename listdir 递归
+'''
