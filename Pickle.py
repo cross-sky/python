90d005843b0a206ba9f4b1052e2c9586308c9b60
cross-sky <xtaosky@live.com>
2015-03-13 22:45:55 +0800

    pickle

---
+++ Pickle.py
@@ -0,0 +1,19 @@
+﻿import  pickle
+
+class Bird(object):
+    have_feather = True
+    way_of_reproduction = 'egg'
+
+summer = Bird()
+apple = [112, 'abs']
+fn = 'e:\dat.txt'
+'''
+pickle load,dump, with as
+ '''
+with open(fn, 'wb') as f:
+    pickle.dump(apple, f)
+#    pickle.dump(apple, f)
+
+with open(fn, 'rb') as f:
+    summer = pickle.load(f)
+    print(summer)
